cd479f8afc195b466717a15c9620873869fd9807
hagger <haggersoma@gmail.com>
2023-08-26 15:25:34 +0300

    add item to view home

--- lib/features/home/presentation/views/widgets/home_viewbody.dart
+++ lib/features/home/presentation/views/widgets/home_viewbody.dart
@@ -1,7 +1,7 @@
 
 import 'package:blookyapp/features/home/presentation/views/widgets/customappbar.dart';
+import 'package:blookyapp/features/home/presentation/views/widgets/customlistviewitem.dart';
 import 'package:flutter/material.dart';
-
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,8 +9,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar(),
-      ],
+      children: [CustomAppBar(), CustomListViewItem()],
     );
   }
 }
