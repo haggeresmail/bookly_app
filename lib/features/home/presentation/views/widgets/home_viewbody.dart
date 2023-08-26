2880d169c32074fbd2a229ad55878832828f1f2c
hagger <haggersoma@gmail.com>
2023-08-26 14:58:32 +0300

    create appbar

---
+++ lib/features/home/presentation/views/widgets/home_viewbody.dart
@@ -0,0 +1,17 @@
+
+import 'package:blookyapp/features/home/presentation/views/widgets/customappbar.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [CustomAppBar(),
+      ],
+    );
+  }
+}
+
