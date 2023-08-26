38c0becc6a6e4cc12c71453838bab2bf172d6d96
hagger <haggersoma@gmail.com>
2023-08-26 15:36:16 +0300

    add listview books

--- lib/features/home/presentation/views/widgets/home_viewbody.dart
+++ lib/features/home/presentation/views/widgets/home_viewbody.dart
@@ -1,4 +1,3 @@
-
 import 'package:blookyapp/features/home/presentation/views/widgets/customappbar.dart';
 import 'package:blookyapp/features/home/presentation/views/widgets/customlistviewitem.dart';
 import 'package:flutter/material.dart';
@@ -9,8 +8,27 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar(), CustomListViewItem()],
+      children: [CustomAppBar(), ListViewItems()],
     );
   }
 }
 
+class ListViewItems extends StatelessWidget {
+  const ListViewItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal:1),
+            child: CustomListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
