ffa1ff7c7e1585dfe37fa07719ac1ddd48e5e7c7
hagger <haggersoma@gmail.com>
2023-08-27 13:30:27 +0300

    best seller item part2

--- lib/features/home/presentation/views/widgets/home_viewbody.dart
+++ lib/features/home/presentation/views/widgets/home_viewbody.dart
@@ -11,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
+      padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -20,6 +20,9 @@ class HomeViewBody extends StatelessWidget {
           Text(
             "Best Seller",
             style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
           ),
           BestSellerListViewItem(),
         ],
@@ -47,8 +50,21 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
-          const Column(
-            children: [],
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  "harry poter and Global of fire",
+                  style: Styles.textStyle20,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              )
+            ],
           )
         ],
       ),
