da9450e619f5cdef3e417e390733132e162bd3f5
hagger <haggersoma@gmail.com>
2023-08-28 12:50:47 +0300

    add title and subtitle to book view detail

--- lib/features/home/presentation/views/widgets/book_detail_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_detail_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:blookyapp/core/utils/styles.dart';
 import 'package:blookyapp/features/home/presentation/views/widgets/book_details_appbar.dart';
 import 'package:blookyapp/features/home/presentation/views/widgets/custom_book_image_item.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,24 @@ class BookDetailsViewBody extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * .17),
             child: const CustomBookImageItem(),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            "The Jungle Book",
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              "Rudyard Kipling❤❤",
+              style: Styles.textStyle18.copyWith(
+                  fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
+            ),
           ),
         ],
       ),
