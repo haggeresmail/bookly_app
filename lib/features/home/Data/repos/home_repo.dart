7ccf82c69be2d91e1d13664788c75b4d7bf1b9fa
hagger <haggersoma@gmail.com>
2023-08-28 16:08:09 +0300

    create home repo method

--- lib/features/home/Data/repos/home_repo.dart
+++ lib/features/home/Data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:blookyapp/core/errors/failuers.dart';
+import 'package:blookyapp/features/home/Data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class  HomeRepo{
-  fetechBestSellerBooks();
-  fetechFeaturedBooks();
+   Future<Either <Failure,List<BookModel>>> fetechBestSellerBooks();
+ Future<Either <Failure,List<BookModel>>> fetechFeaturedBooks();
 }
