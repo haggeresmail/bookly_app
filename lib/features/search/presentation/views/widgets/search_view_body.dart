c68d31a1977bec25ad5a561c813bda195591125a
hagger <haggersoma@gmail.com>
2023-08-28 15:24:56 +0300

    search result view ui done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,17 +1,44 @@
+import 'package:blookyapp/core/utils/styles.dart';
+import 'package:blookyapp/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:blookyapp/features/search/presentation/views/widgets/custom_search_textfield.dart';
 import 'package:flutter/material.dart';
-
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [CustomSearchTextField(),
-
-    ],
-
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomSearchTextField(),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          "Search Results ",
+          style: Styles.textStyle18,
+        ),
+        Expanded(child: SearchResultListView()),
+      ],
     );
   }
 }
 
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BestSellerListViewItem(),
+        );
+      },
+    );
+  }
+}
