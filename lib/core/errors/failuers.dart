7cf6af8badd56d5815c8f5a7e7e60f56569e07bc
hagger <haggersoma@gmail.com>
2023-08-29 13:18:40 +0300

    create serer failuer from dio error

--- lib/core/errors/failuers.dart
+++ lib/core/errors/failuers.dart
@@ -1,4 +1,68 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
 abstract class Failure{
+  final String errMessage;
+
+  Failure(this.errMessage);
 
 }
-class ServerFailure extends Failure{}
+class ServerFailure extends Failure{
+  ServerFailure(super.errMessage);
+  factory ServerFailure.fromDioError(DioError dioError){
+    switch(dioError.type){
+      
+      case DioErrorType.connectionTimeout:
+        // TODO: Handle this case.
+        return ServerFailure("connection Time Out with api server");
+      case DioErrorType.sendTimeout:
+        // TODO: Handle this case.
+         return ServerFailure( "send Time Out with api server");
+      case DioErrorType.receiveTimeout:
+       return ServerFailure( "receive Time Out with api server");
+        // TODO: Handle this case.
+      case DioErrorType.badCertificate:
+      return ServerFailure("receive Time Out with api server");
+      
+        // TODO: Handle this case.
+      case DioErrorType.badResponse:
+      return ServerFailure.fromResponse(dioError.response!.statusCode!, dioError.response!.data);
+        // TODO: Handle this case.
+      case DioErrorType.cancel:
+      return ServerFailure("request with api server cancelled");
+        // TODO: Handle this case.
+      case DioErrorType.connectionError:
+       return ServerFailure("connectionError with api server cancelled");
+        // TODO: Handle this case.
+      case DioErrorType.unknown:
+      if(dioError.message!.contains('SocketException')){
+        return ServerFailure("no internet");
+      }
+       return ServerFailure(" error plz,try again later");
+      default:
+       return ServerFailure("Oops there was an error");
+        // TODO: Handle this case.
+    }
+  
+      
+      
+    }
+    factory ServerFailure.fromResponse(int statuescode , dynamic response){
+      if (statuescode==400|| statuescode==401 || statuescode==403){
+        return ServerFailure(response['error']['message']);
+      }
+      else if(statuescode==404){
+        return ServerFailure("method not found");
+      }
+      else if(statuescode==500){
+        return ServerFailure("internal server error");
+      }
+      else {
+        return ServerFailure("Oops there was an error");
+      }
+
+
+    }
+}
--- lib/features/home/Data/repos/home_repo_imp.dart
+++ lib/features/home/Data/repos/home_repo_imp.dart
@@ -3,6 +3,7 @@ import 'package:blookyapp/core/utils/api_service.dart';
 import 'package:blookyapp/features/home/Data/models/book_model/book_model.dart';
 import 'package:blookyapp/features/home/Data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImp extends HomeRepo {
   final ApiService apiService;
@@ -21,7 +22,10 @@ class HomeRepoImp extends HomeRepo {
            }
            return right(books);
 } catch (e) {
-  return left(ServerFailure());
+  if(e is DioError){
+  return left(ServerFailure.fromDioError(e));
+  }
+  return left(ServerFailure(e.toString()));
   // TODO
 }
   }
