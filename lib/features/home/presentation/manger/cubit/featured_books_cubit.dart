6639d92de1926b6cf644e6f0fa29a3b879d3760e
hagger <haggersoma@gmail.com>
2023-08-29 13:44:39 +0300

    create futured books method

--- lib/features/home/presentation/manger/cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/cubit/featured_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:blookyapp/features/home/Data/models/book_model/book_model.dart';
+import 'package:blookyapp/features/home/Data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetachFeaturedBooks()async{
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetechFeaturedBooks();
+    result.fold((failure)  {
+      emit(FeaturedBooksFailure(failure.errMessage)); 
+    }, (books)  {
+emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
