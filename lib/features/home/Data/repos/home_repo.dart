4b21d781e20bc3c866928d6660b9ce62aac478e8
hagger <haggersoma@gmail.com>
2023-08-29 12:36:40 +0300

    implement fetch newest books

--- lib/features/home/Data/repos/home_repo.dart
+++ lib/features/home/Data/repos/home_repo.dart
@@ -2,7 +2,7 @@ import 'package:blookyapp/core/errors/failuers.dart';
 import 'package:blookyapp/features/home/Data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class  HomeRepo{
-   Future<Either <Failure,List<BookModel>>> fetechBestSellerBooks();
- Future<Either <Failure,List<BookModel>>> fetechFeaturedBooks();
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetechNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetechFeaturedBooks();
 }
--- lib/features/home/Data/repos/home_repo_imp.dart
+++ lib/features/home/Data/repos/home_repo_imp.dart
@@ -1,13 +1,29 @@
 import 'package:blookyapp/core/errors/failuers.dart';
+import 'package:blookyapp/core/utils/api_service.dart';
 import 'package:blookyapp/features/home/Data/models/book_model/book_model.dart';
 import 'package:blookyapp/features/home/Data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImp extends HomeRepo{
+class HomeRepoImp extends HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImp({required this.apiService});
   @override
-  Future<Either<Failure, List<BookModel>>> fetechBestSellerBooks() {
-    // TODO: implement fetechBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetechNewestBooks() async{
+   try {
+  var data=  await apiService.get(
+       endpoint:
+           'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:Programming');
+           List<BookModel> books=[];
+           for (var item in data["items"]) {
+            books.add(BookModel.fromJson(item));
+             
+           }
+           return right(books);
+} catch (e) {
+  return left(ServerFailure());
+  // TODO
+}
   }
 
   @override
@@ -15,5 +31,4 @@ class HomeRepoImp extends HomeRepo{
     // TODO: implement fetechFeaturedBooks
     throw UnimplementedError();
   }
-
 }
